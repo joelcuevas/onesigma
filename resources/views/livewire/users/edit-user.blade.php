07aef823c9cccad77d622cb2b759a8d2b30fb77e
Joel Cuevas <hola@joelcuevas.com>
2024-01-27 23:03:18 -0600

    Updated form layout in edit users

--- resources/views/livewire/users/edit-user.blade.php
+++ resources/views/livewire/users/edit-user.blade.php
@@ -12,37 +12,59 @@
     <div class="space-y-6">
         <div class="x-card">
             <form wire:submit="save">
-                <div class="space-y-12">
-                    <div class="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-gray-900/10 pb-12 md:grid-cols-3">
-                        <div>
-                            <h2 class="font-medium text-gray-900">Perfil Profesional</h2>
-                            <p class="mt-2 leading-6 text-gray-600">Datos oficiales de la organización. Los ingenieros no pueden modificarlos en su perfil personal.</p>
-                        </div>
+                <div class="space-y-6 border-b border-gray-900/10 pb-8">
+                    <h2 class="text-lg font-bold text-gray-900">
+                        {{ __('Perfil del Usuario') }}
+                    </h2>
 
-                        <div class="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 md:col-span-2 md:grid-cols-6">
-                            <div class="md:col-span-4">
-                                <x-input-label>{{ __('Nombre') }}</x-input-label>
+                    <div>
+                        <x-input-label>{{ __('Nombre') }}</x-input-label>
+                        <div class="grid grid-cols-1 gap-x-12 gap-y-3 lg:grid-cols-12">
+                            <div class="lg:col-span-5">
                                 <x-text-input wire:model="name" />
                                 <x-input-error :messages="$errors->get('name')" />
                             </div>
+                            <div class="flex pt-2 text-gray-500 lg:col-span-7 lg:items-start">
+                                <x-heroicon-o-question-mark-circle class="mr-2 h-5 w-5 min-w-5" />
+                                {{ __('El usuario puede modificarlo desde su perfil.') }}
+                            </div>
+                        </div>
+                    </div>
 
-                            <div class="md:col-span-4">
-                                <x-input-label>{{ __('Email') }}</x-input-label>
+                    <div>
+                        <x-input-label>{{ __('Email') }}</x-input-label>
+                        <div class="grid grid-cols-1 gap-x-12 gap-y-3 lg:grid-cols-12">
+                            <div class="lg:col-span-5">
                                 <x-text-input wire:model="email" />
                                 <x-input-error :messages="$errors->get('email')" />
                             </div>
+                            <div class="flex pt-2 text-gray-500 lg:col-span-7 lg:items-start">
+                                <x-heroicon-o-question-mark-circle class="mr-2 h-5 w-5 min-w-5" />
+                                {{ __('El usuario puede modificarlo. Puede ser usado para iniciar sesion con Github.') }}
+                            </div>
+                        </div>
+                    </div>
 
-                            @if (! $user->exists || $user->hasPassword())
-                                <div class="md:col-span-4">
-                                    <x-input-label>{{ __('Contraseña') }}</x-input-label>
+                    @if (! $user->exists || $user->hasPassword())
+                        <div>
+                            <x-input-label>{{ __('Contraseña') }}</x-input-label>
+                            <div class="grid grid-cols-1 gap-x-12 gap-y-3 lg:grid-cols-12">
+                                <div class="lg:col-span-5">
                                     <x-text-input wire:model="password" />
                                     <x-input-error :messages="$errors->get('password')" />
                                 </div>
-                            @endif
+                                <div class="flex pt-2 text-gray-500 lg:col-span-7 lg:items-start">
+                                    <x-heroicon-o-question-mark-circle class="mr-2 h-5 w-5 min-w-5" />
+                                    {{ __('Solo si desea que el usuario inicie sesión contraseña; vacío si no.') }}
+                                </div>
+                            </div>
+                        </div>
+                    @endif
 
-                            <div class="md:col-span-3">
-                                <x-input-label>{{ __('Rol') }}</x-input-label>
-
+                    <div>
+                        <x-input-label>{{ __('Rol') }}</x-input-label>
+                        <div class="grid grid-cols-1 gap-x-12 gap-y-3 lg:grid-cols-12">
+                            <div class="lg:col-span-5">
                                 <x-select-input wire:model="role">
                                     @foreach ($allRoles as $role)
                                         <option value="{{ $role->value }}">{{ $role->name }}</option>
@@ -51,45 +73,52 @@
 
                                 <x-input-error :messages="$errors->get('role')" />
                             </div>
+                            <div class="flex pt-2 text-gray-500 lg:col-span-7 lg:items-start">
+                                <x-heroicon-o-question-mark-circle class="mr-2 h-5 w-5 min-w-5" />
+                                {{ __('Importante: los admins pueden crear otros usuarios.') }}
+                            </div>
                         </div>
                     </div>
                 </div>
 
-                <div class="mt-6 flex items-center justify-end gap-x-6">
+                <div class="mt-8 flex items-center gap-x-6">
+                    <x-primary-button type="submit">Guardar</x-primary-button>
                     <a href="{{ route('users') }}" class="hover:underline">
                         {{ __('Cancelar') }}
                     </a>
-                    <x-primary-button type="submit">Guardar</x-primary-button>
                 </div>
             </form>
         </div>
 
         @if ($this->user->exists)
             <div class="x-card">
-                <div class="space-y-4">
-                    <h2 class="font-medium text-gray-900">{{ __('Eliminar Usuario') }}</h2>
-                    <p class="text-gray-600">Once the account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.</p>
+                <div class="space-y-5">
+                    <h2 class="font-bold text-lg text-red-800">{{ __('Eliminar Usuario') }}</h2>
+                    <p class="text-red-800 max-w-2xl font-medium">
+                        {{ __('Una vez que la cuenta sea borrada, se eliminará permanentemente toda la información y recursos del usuario. Esta acción no se puede deshacer.') }}
+                    </p>
+                    
                     <x-danger-button x-data="" x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')">
-                        {{ __('Delete Account') }}
+                        {{ __('Eliminar Usuario') }}
                     </x-danger-button>
 
                     <x-modal name="confirm-user-deletion" :show="$errors->isNotEmpty()" focusable>
                         <form wire:submit="delete" class="p-6">
-                            <h2 class="text-lg font-medium text-gray-900">
-                                {{ __('Are you sure you want to delete the user?') }}
+                            <h2 class="text-lg font-medium text-gray-900 mb-4">
+                                {{ __('¿Confirmas que deseas eliminar al usuario?') }}
                             </h2>
 
-                            <p class="mt-1 text-sm text-gray-600">
-                                {{ __('Once the account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
+                            <p class="mt-1 text-gray-600">
+                                {{ __('Una vez que la cuenta sea borrada, se eliminará permanentemente toda la información y recursos del usuario. Esta acción no se puede deshacer.') }}
                             </p>
 
                             <div class="mt-6 flex justify-end">
                                 <x-secondary-button x-on:click="$dispatch('close')">
-                                    {{ __('Cancel') }}
+                                    {{ __('Cancelar') }}
                                 </x-secondary-button>
 
                                 <x-danger-button class="ms-3">
-                                    {{ __('Delete Account') }}
+                                    {{ __('Eliminar Usuario') }}
                                 </x-danger-button>
                             </div>
                         </form>
